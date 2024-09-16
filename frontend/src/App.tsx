f939c3d9ca95ee832989de112cb4d117db625ff6
leonardo <leonardo.palvarenga@gmail.com>
2024-09-16 10:14:20 -0300

    initials dependencies for frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { Grafico } from './components/myComponents/grafico';
+export function App() {
+
+  return (
+    <div className=''>
+      <Grafico/>
+    </div>
+  );
+}
---
+++ frontend/src/components/myComponents/grafico.tsx
@@ -0,0 +1,119 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+import { useEffect, useState } from "react"
+
+export const description = "A stacked area chart"
+
+const chartData = [
+  { month: "January", income: 186.00, outcome: 80.00 },
+  { month: "February", income: 305.00, outcome: 200.00 },
+  { month: "March", income: 237.00, outcome: 120.00 },
+  { month: "April", income: 73.00, outcome: 190.00 },
+  { month: "May", income: 209.00, outcome: 130.00 },
+  { month: "June", income: 214.50, outcome: 140.00 },
+]
+
+const chartConfig = {
+  income: {
+    label: "Income",
+    color: "hsl(var(--chart-2))",
+  },
+  outcome: {
+    label: "Outcome",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig
+
+export function Grafico() {
+  const [ totalIncome, setTotalIncome ] = useState<number>(0)
+
+  function calculaTotalIncome() {
+    const total = chartData.reduce((acc, item) => acc + item.income, 0);
+    setTotalIncome(total)
+  }
+
+  useEffect(() => {
+    calculaTotalIncome();
+  }, [])
+
+  return (
+    <Card className="w-[400px] ">
+      <CardHeader>
+        <CardTitle>Gráfico de Gerenciamento de finança</CardTitle>
+        <CardDescription>
+          Suas entradas e saídas
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="dot" />}
+            />
+            <Area
+              dataKey="outcome"
+              type="basis"
+              fill="var(--color-outcome)"
+              fillOpacity={0.4}
+              stroke="var(--color-outcome)"
+              stackId="a"
+            />
+              <Area
+                dataKey="income"
+                type="basis"
+                fill="var(--color-income)"
+                fillOpacity={0.4}
+                stroke="var(--color-income)"
+                stackId="a"
+              />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-start gap-2 text-sm">
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2 font-medium leading-none">
+              Total Income: {totalIncome} <TrendingUp className="h-4 w-4" />
+            </div>
+            <div className="flex items-center gap-2 leading-none text-muted-foreground">
+              January - June 2024
+            </div>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
